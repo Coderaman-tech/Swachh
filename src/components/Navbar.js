82f822d5fae1285fc9ed036a1cab5e35ed20efcd
Aman Kumar Shrivastava <amankumarshrivastava835@gmail.com>
2023-03-22 20:03:54 +0530

    add nav

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      
+<ul id='navbar'>
+  <li><a className="active" href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+
+    </div>
+  )
+}
+
+export default Navbar
