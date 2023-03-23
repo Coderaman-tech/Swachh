fc734aa2b33f8c03c7bf1da25709ab361bc734a6
Aman Kumar Shrivastava <amankumarshrivastava835@gmail.com>
2023-03-23 23:51:51 +0530

    update nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,15 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>
-      
-<ul id='navbar'>
-  <li><a className="active" href="#home">Home</a></li>
-  <li><a href="#news">News</a></li>
-  <li><a href="#contact">Contact</a></li>
-  <li><a href="#about">About</a></li>
-</ul>
+    <div id='navbar-container'>
+
+      <ul id='navbar'>
+        <li><a href="#news">News</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a className="active" href="#home">Home</a></li>
+
+      </ul>
 
     </div>
   )
