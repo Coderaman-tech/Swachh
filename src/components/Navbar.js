56abcc2605b6343ec599efe0051f2ff30de9d155
Aman Kumar Shrivastava <amankumarshrivastava835@gmail.com>
2023-03-24 22:45:32 +0530

    update nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
         <li><a href="#contact">Contact</a></li>
         <li><a href="#about">About</a></li>
         <li><a className="active" href="#home">Home</a></li>
-
+        <li><a href="#home-page" id='title'>Swachh</a></li>
       </ul>
 
     </div>
