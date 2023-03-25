5768cbd43e77b03ffa82c1893353a5f17c3378d7
Aman Kumar Shrivastava <amankumarshrivastava835@gmail.com>
2023-03-25 22:55:31 +0530

    add logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,20 @@
-import React from 'react'
-
+import React from 'react';
 function Navbar() {
   return (
-    <div id='navbar-container'>
-
+    // public\static\images\logo.png
       <ul id='navbar'>
+       <li>
+        <a href='/'>
+          <img src='./static/images/logo.png' alt=''/>
+          Swachh
+        </a>
+       </li>
+        <li><a href="#home">Home</a></li>
         <li><a href="#news">News</a></li>
         <li><a href="#contact">Contact</a></li>
         <li><a href="#about">About</a></li>
-        <li><a className="active" href="#home">Home</a></li>
-        <li><a href="#home-page" id='title'>Swachh</a></li>
       </ul>
-
-    </div>
+    
   )
 }
 
